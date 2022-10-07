08e9ef8938db0add813aa58303c8c9909e910267
joshishruty99 <63907554+joshishruty99@users.noreply.github.com>
2022-10-07 19:38:39 +0530

    Update text2speech.py

--- text2speech.py
+++ text2speech.py
@@ -4,10 +4,10 @@ from gtts import gTTS
   
 # import Os module to start the audio file
 import os 
-mytext = "This is a test line."
+mytext = "This is a test message."
 # Language we want to use 
 language = 'en'
 myobj = gTTS(text=mytext, lang=language, slow=False) 
-myobj.save("mytext.mp3") 
+myobj.save("generated.mp3") 
 # Play the converted file 
 os.system("start output.mp3")
